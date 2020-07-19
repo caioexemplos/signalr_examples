e736f42409fc74dadf84f7c23544f3e00ad3792a
DevCaioSchool <caio.treinamentos@gmail.com>
2020-07-19 14:01:16 -0300

    First commit

---
+++ SignalR/App.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    var app = angular.module('messageBox', []);
+    app.value('$', $);
+
+    app.factory('noteService', ['$', '$rootScope',
+    function ($, $rootScope) {
+        var proxy;
+        var connection;
+        return {
+            connect: function () {
+                connection = $.hubConnection();
+                proxy = connection.createHubProxy('HubMessage');
+                connection.start();
+                proxy.on('messageAdded', function (remetente, destinatario, message) {
+                    $rootScope.$broadcast('messageAdded', remetente, destinatario, message);
+                });
+            },
+            isConnecting: function () {
+                return connection.state === 0;
+            },
+            isConnected: function () {
+                return connection.state === 1;
+            },
+            connectionState: function () {
+                return connection.state;
+            },
+            sendMessage: function (remetente, destinatario, message) {
+                proxy.invoke('SendMessage', remetente, destinatario, message);
+            },
+        }
+    }]);
+
+    app.controller('messageController', function ($scope, noteService) {
+                
+        noteService.connect();
+        $scope.messages = [];
+
+        $scope.$on('messageAdded', function (event, remetente, destinatario, message) {
+            var mensagem = { de: remetente, para: destinatario, mensagem: message };
+            $scope.messages.push(mensagem);
+            $scope.$apply();
+        });
+
+        $scope.sendMessage = function () {
+            noteService.sendMessage($scope.remetente, $scope.destinatario, $scope.mensagem);
+        };
+    });
+})()
---
+++ SignalR/MyHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalR
+{
+    [HubName("HubMessage")]
+    public class MyHub : Hub
+    {
+        public void SendMessage(string remetente, string destinatario, string message)
+        {
+            Clients.All.messageAdded(remetente, destinatario, message);
+        }
+        
+        public void SendMessageOfficial(string name,string mensagem)
+        {
+            Clients.All.messageOfficial(name, mensagem);
+        }
+    }
+}
